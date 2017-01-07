40bdf5f8f9a715e32fd1770d064e25cbde6b53a5
Rafael Malave <rafomalave@gmail.com>
2017-01-07 23:42:16 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,9 +23,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                email: "user@example.com",
                                password: "password",
                                password_confirmation: "password" } }
-      end
+    end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
   end
   
